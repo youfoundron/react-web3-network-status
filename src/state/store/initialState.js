5720c1a6300b770e5cc4995e5b14f6903e04a52f
Ron Gierlach <rongierlach@gmail.com>
2017-09-07 16:17:48 -0500

    add initialState

---
+++ src/state/store/initialState.js
@@ -0,0 +1,24 @@
+// @flow
+type State = {
+  +web3: {},
+  +network: {
+    +id: number,
+    +listening: bool,
+    +peerCount: number
+  },
+  +accounts: Array<string>,
+  +defaultAccount: string
+};
+
+const initialState: State = {
+  web3: {},
+  network: {
+    id: null,
+    listening: false,
+    peerCount: null
+  },
+  accounts: [],
+  defaultAccount: null
+}
+
+export default initialState
