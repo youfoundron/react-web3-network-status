3d7e23bc656ff7334f3e3f1379d8090cbe70066d
Ron Gierlach <rongierlach@gmail.com>
2017-09-07 16:21:10 -0500

    stub demo App

---
+++ demo/js/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import NetworkStatus from '../../src'
+
+const App = () => (
+  <main>
+    <header>
+      <NetworkStatus />
+    </header>
+    <h1 className='tc'>Hello World</h1>
+  </main>
+)
+
+export default App
