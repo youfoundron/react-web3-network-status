956654402708f07ef4c366ac73914292253f62d7
Ron Gierlach <rongierlach@gmail.com>
2017-09-07 16:13:13 -0500

    webpack-dev-server config

---
+++ webpack.server.babel.js
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import makeRule from 'webpack-make-rule'
+import { HotModuleReplacementPlugin } from 'webpack'
+
+const demoDir = resolve(__dirname, 'demo')
+
+export default {
+  entry: resolve(demoDir, 'js/index.js'),
+  output: {
+    path: demoDir,
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      makeRule(/\.jsx?$/, ['react-hot-loader', 'babel-loader']),
+      makeRule(/\.(html|ico)$/, 'file-loader?name=[name].[ext]')
+    ]
+  },
+  plugins: [
+    new HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    hot: true,
+    port: 3000,
+    inline: true,
+    contentBase: demoDir
+  }
+}
