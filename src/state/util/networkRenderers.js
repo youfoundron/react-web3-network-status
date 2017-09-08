2b2442278d11d373471fab1e413c54000be59dd9
Ron Gierlach <rongierlach@gmail.com>
2017-09-08 18:57:45 -0500

    add networkRenderers util

---
+++ src/state/util/networkRenderers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import networksConfig from '../../networksConfig'
+import NetworkStatus from '../../components/NetworkStatus'
+
+const accumulateRenderers = (renderers, {id, ...networkConfig}) => {
+  renderers[id] = props => <NetworkStatus
+    {...props}
+    {...networkConfig}
+  />
+  return renderers
+}
+
+export default networksConfig.reduce(accumulateRenderers, {})
