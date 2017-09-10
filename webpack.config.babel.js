60d9af35219fd314e591e23df009d372ef48f63c
Ron Gierlach <rongierlach@gmail.com>
2017-09-10 14:12:56 -0500

    add react and web3 as externals

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -10,6 +10,20 @@ export default {
     library: 'ReactWeb3NetworkStatus',
     libraryTarget: 'umd'
   },
+  externals: {
+    'react': {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'web3': {
+      commonjs: 'web3',
+      commonjs2: 'web3',
+      amd: 'web3',
+      root: 'Web3'
+    }
+  },
   module: {
     rules: [ makeRule(/\.jsx?$/, 'babel-loader') ]
   },
