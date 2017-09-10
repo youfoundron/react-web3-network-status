bfeb3d8cf09a94778cc0589c1c0388d5a87c872f
Ron Gierlach <rongierlach@gmail.com>
2017-09-10 10:52:28 -0500

    remove externals from webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
-import makeRule from 'webpack-make-rule'
 import { optimize } from 'webpack'
+import makeRule from 'webpack-make-rule'
 
 export default {
   entry: resolve(__dirname, './src/index.js'),
@@ -10,32 +10,6 @@ export default {
     library: 'ReactWeb3NetworkStatus',
     libraryTarget: 'umd'
   },
-  // externals: {
-  //   'react': {
-  //     commonjs: 'react',
-  //     commonjs2: 'react',
-  //     amd: 'react',
-  //     root: 'React'
-  //   },
-  //   'redux': {
-  //     commonjs: 'redux',
-  //     commonjs2: 'redux',
-  //     amd: 'redux',
-  //     root: 'Redux'
-  //   },
-  //   'react-redux': {
-  //     commonjs: 'react-redux',
-  //     commonjs2: 'react-redux',
-  //     amd: 'react-redux',
-  //     root: 'ReactRedux'
-  //   },
-  //   'redux-saga': {
-  //     commonjs: 'redux-saga',
-  //     commonjs2: 'redux-saga',
-  //     amd: 'redux-saga',
-  //     root: 'ReduxSaga'
-  //   }
-  // },
   module: {
     rules: [ makeRule(/\.jsx?$/, 'babel-loader') ]
   },
