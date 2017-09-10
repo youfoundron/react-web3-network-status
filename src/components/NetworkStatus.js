b98ec993701f0f1ca448e710318483fd7971e3b2
Ron Gierlach <rongierlach@gmail.com>
2017-09-09 21:40:48 -0500

    update NetworkStatus to use NetworkLink

--- src/components/NetworkStatus.js
+++ src/components/NetworkStatus.js
@@ -1,25 +1,14 @@
 import React from 'react'
 
+import NetworkLink from './NetworkLink'
 import NetworkIcon from './NetworkIcon'
 import NetworkLabel from './NetworkLabel'
 
 const NetworkStatus = ({iconStyles, name, type, color, ...props}) => (
-  <div id='__network-status__' {...props}>
+  <NetworkLink {...props}>
     <NetworkIcon iconStyles={iconStyles} />
     <NetworkLabel name={name} type={type} color={color} />
-    <style jsx>{`
-      #__network-status__ {
-        display: flex;
-        align-items: center;
-        line-height: 1em;
-        font-size: 12px
-        font-weight: 700;
-        font-family: helvetica, arial, sans-serif
-        text-transform: capitalize;
-        text-decoration: none;
-      }
-    `}</style>
-  </div>
+  </NetworkLink>
 )
 
 export default NetworkStatus
