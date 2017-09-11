e09821158e9f8094e7899fafae1d31b06904149a
Ron Gierlach <rongierlach@gmail.com>
2017-09-10 20:39:18 -0500

    add flow typing for network reducer

--- src/state/reducers/network.js
+++ src/state/reducers/network.js
@@ -1,9 +1,11 @@
-import types from '../constants/actionTypes'
+// @flow
 import initialState from '../store/initialState'
+import types from '../constants/actionTypes'
+import type { Action } from '../constants/actionTypes'
 
 const { network: defaultState } = initialState
 
-export default (state = defaultState, { type, payload }) => {
+export default (state = defaultState, { type, payload }: Action) => {
   switch (type) {
     case types.GET_NETWORK_ID_SUCCESS:
       return { ...state, id: payload }
