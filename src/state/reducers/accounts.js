e3956f96a58bb87c587035dfaf67518f1299e01e
Ron Gierlach <rongierlach@gmail.com>
2017-09-10 20:39:32 -0500

    add flow typing for accounts reducer

--- src/state/reducers/accounts.js
+++ src/state/reducers/accounts.js
@@ -1,9 +1,11 @@
-import types from '../constants/actionTypes'
+// @flow
 import initialState from '../store/initialState'
+import types from '../constants/actionTypes'
+import type { Action } from '../constants/actionTypes'
 
 const { accounts: defaultState } = initialState
 
-export default (state = defaultState, { type, payload }) => {
+export default (state = defaultState, { type, payload }: Action) => {
   switch (type) {
     case types.GET_ACCOUNTS_SUCCESS:
       return payload
