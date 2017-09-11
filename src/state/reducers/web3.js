cbf8b736f2f22b2a304c83acf08309487dd0b250
Ron Gierlach <rongierlach@gmail.com>
2017-09-10 20:39:02 -0500

    add flow typing for web3 reducer

--- src/state/reducers/web3.js
+++ src/state/reducers/web3.js
@@ -1,9 +1,11 @@
-import types from '../constants/actionTypes'
+// @flow
 import initialState from '../store/initialState'
+import types from '../constants/actionTypes'
+import type { Action } from '../constants/actionTypes'
 
 const { web3: defaultState } = initialState
 
-export default (state = defaultState, { type, payload }) => {
+export default (state = defaultState, { type, payload }: Action) => {
   switch (type) {
     case types.RECEIVE_WEB3_INSTANCE:
       return payload
