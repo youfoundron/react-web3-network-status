7f4ab3805f8ac1a79ec6eae976d560c8fae3b678
Ron Gierlach <rongierlach@gmail.com>
2017-09-10 20:37:07 -0500

    add flow typing for NetworkLink

--- src/components/NetworkLink.js
+++ src/components/NetworkLink.js
@@ -1,7 +1,15 @@
+// @flow
 import React from 'react'
 import { connect } from 'react-redux'
 
-const NetworkLink = ({etherScanUrl, defaultAccount, dispatch, children, ...props}) => (
+type Props = {
+  etherScanUrl: string,
+  defaultAccount: string,
+  dispatch: ({}) => void,
+  children?: React.Node
+}
+
+const NetworkLink = ({etherScanUrl, defaultAccount, dispatch, children, ...props}: Props) => (
   etherScanUrl && defaultAccount
   ? <a id='__network-status__' target='_blank' {...props}
     href={`${etherScanUrl}/address/${defaultAccount}`}
